00f682b05b9a2e26981739ac978f555bb218f519
XCrone <ls.xcrone@gmail.com>
2023-02-10 11:47:43 +0300

    feature: add cors

--- src/main.ts
+++ src/main.ts
@@ -4,7 +4,12 @@ import { AppModule } from './app.module';
 const PORT = process.env.SERVER_PORT;
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  const app = await NestFactory.create(AppModule, {
+    cors: {
+      origin: 'http://localhost:3000',
+      credentials: true,
+    },
+  });
   await app.listen(PORT || 4443);
 }
 bootstrap();
