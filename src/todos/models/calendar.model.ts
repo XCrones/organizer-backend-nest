5c15d60e130579e73698da90e5dc1e7c503b9f6f
XCrone <ls.xcrone@gmail.com>
2023-02-10 11:46:19 +0300

    feature: create model

---
+++ src/todos/models/calendar.model.ts
@@ -0,0 +1,34 @@
+import { Model, Table, Column } from 'sequelize-typescript';
+
+@Table
+export class Todo extends Model {
+  @Column
+  uid: number;
+
+  @Column
+  year: number;
+
+  @Column
+  month: number;
+
+  @Column
+  day: number;
+
+  @Column
+  timeStart: string;
+
+  @Column
+  timeEnd: string;
+
+  @Column
+  title: string;
+
+  @Column
+  description: string;
+
+  @Column
+  color: string;
+
+  @Column
+  background: string;
+}
