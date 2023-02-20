c3813f1af1bc9bacf4a64b09e60fa2fb57395e66
XCrone <ls.xcrone@gmail.com>
2023-02-20 18:10:25 +0300

    feature: add validators

--- src/todos/dto/update-todo.dto.ts
+++ src/todos/dto/update-todo.dto.ts
@@ -1,24 +1,27 @@
-import { IsNotEmpty } from 'class-validator';
+import { IsBoolean, IsNotEmpty, IsNumber, IsString } from 'class-validator';
 
 export class UpdateTodoDto {
   @IsNotEmpty()
+  @IsString()
   readonly title: string;
 
   @IsNotEmpty()
+  @IsString()
   readonly category: string;
 
   @IsNotEmpty()
+  @IsNumber()
   readonly priority: number;
 
   @IsNotEmpty()
+  @IsString()
   readonly deadline: Date;
 
   @IsNotEmpty()
+  @IsBoolean()
   readonly status: boolean;
 
   @IsNotEmpty()
-  readonly descritption: string;
-
-  @IsNotEmpty()
+  @IsString()
   readonly background: string;
 }
