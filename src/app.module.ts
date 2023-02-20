12e2e9e425cb117531bc65d1aa5a2c09cb832ddf
XCrone <ls.xcrone@gmail.com>
2023-02-20 16:37:12 +0300

    feature: add config

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,26 +1,25 @@
 import { SequalizeConfigService } from './config/sequalizeConfig.service';
 import { SequelizeModule } from '@nestjs/sequelize/dist';
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
 import { ConfigModule } from '@nestjs/config';
 import { databaseConfig } from './config';
 import { TodosModule } from './todos/todos.module';
 import { CalendarModule } from './calendar/calendar.module';
+import appConfig from './config';
 
 @Module({
   imports: [
+    ConfigModule.forRoot({
+      load: [databaseConfig, appConfig],
+    }),
     SequelizeModule.forRootAsync({
       imports: [ConfigModule],
       useClass: SequalizeConfigService,
     }),
-    ConfigModule.forRoot({
-      load: [databaseConfig],
-    }),
     TodosModule,
     CalendarModule,
   ],
-  controllers: [AppController],
-  providers: [AppService],
+  controllers: [],
+  providers: [],
 })
 export class AppModule {}
