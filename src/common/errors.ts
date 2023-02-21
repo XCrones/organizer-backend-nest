b1bd0a527a7c1e76ec30c54da74cd2481d03852b
XCrone <ls.xcrone@gmail.com>
2023-02-21 17:36:45 +0300

    feature: add errors (todo, events)

--- src/common/errors.ts
+++ src/common/errors.ts
@@ -1,4 +1,6 @@
 export const APP_ERRORS = {
   EMAIL_BUSY: 'Email is busy',
   AUTH_INCORRECT: 'Incorrect email or password',
+  TODO_NOT_FOUND: 'Todo not found',
+  EVENT_NOT_FOUND: 'Even not found',
 };
