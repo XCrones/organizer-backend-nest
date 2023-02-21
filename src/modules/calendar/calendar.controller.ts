cd1f25e4f84e922bb03b907c0668cf89d1fc3c73
XCrone <ls.xcrone@gmail.com>
2023-02-21 17:37:57 +0300

    feature: add guards and refactor

--- src/modules/calendar/calendar.controller.ts
+++ src/modules/calendar/calendar.controller.ts
@@ -9,12 +9,16 @@ import {
   Param,
   Patch,
   Post,
+  Req,
+  UseGuards,
 } from '@nestjs/common';
 import { UpdateEventDTO } from './dto/update-event.dto';
 import { CreateEventDTO } from './dto/create-event.dto';
 import { CalendarService } from './calendar.service';
 import { ApiResponse, ApiTags } from '@nestjs/swagger';
 import { CalendarResponse } from './response/calendar.response';
+import { JwtAuthGuard } from 'src/guards/jwt.guard';
+import { UserDTO } from '../auth/dto/user.dto';
 
 @Controller('calendar')
 export class CalendarController {
@@ -22,51 +26,68 @@ export class CalendarController {
 
   @ApiTags('API')
   @ApiResponse({ status: HttpStatus.OK, type: Array<CalendarResponse> })
+  @UseGuards(JwtAuthGuard)
   @Header('Content-type', 'application/json')
   @HttpCode(HttpStatus.OK)
   @Get()
-  getEvents(): Promise<CalendarResponse[]> {
-    return this.calendarService.getEvents();
+  getEvents(@Req() request): Promise<CalendarResponse[]> {
+    const user: UserDTO = request.user;
+    return this.calendarService.getEvents(user.id);
   }
 
   @ApiTags('API')
   @ApiResponse({ status: HttpStatus.OK, type: CalendarResponse })
+  @UseGuards(JwtAuthGuard)
   @Header('Content-type', 'application/json')
   @HttpCode(HttpStatus.OK)
   @Get(':id')
-  getOneEvent(@Param('id') id: string): Promise<CalendarResponse> {
-    return this.calendarService.getOneEvent(id);
+  getOneEvent(
+    @Param('id') id: string,
+    @Req() request,
+  ): Promise<CalendarResponse> {
+    const user: UserDTO = request.user;
+    return this.calendarService.getOneEvent(+id, user.id);
   }
 
   @ApiTags('API')
   @ApiResponse({ status: HttpStatus.OK, type: CalendarResponse })
-  @HttpCode(HttpStatus.CREATED)
+  @UseGuards(JwtAuthGuard)
   @Header('Content-type', 'application/json')
+  @HttpCode(HttpStatus.CREATED)
   @Post()
   createEvent(
     @Body() createEventDto: CreateEventDTO,
+    @Req() request,
   ): Promise<CalendarResponse> {
-    return this.calendarService.createEvent(createEventDto);
+    const user: UserDTO = request.user;
+    return this.calendarService.createEvent(createEventDto, user.id);
   }
 
   @ApiTags('API')
   @ApiResponse({ status: HttpStatus.OK, type: CalendarResponse })
+  @UseGuards(JwtAuthGuard)
   @Header('Content-type', 'application/json')
   @HttpCode(HttpStatus.OK)
-  @Patch(':id')
+  @Patch()
   updateEvent(
     @Body() updateEventDto: UpdateEventDTO,
-    @Param('id') id: string,
+    @Req() request,
   ): Promise<CalendarResponse> {
-    return this.calendarService.updateEvent(updateEventDto, id);
+    const user: UserDTO = request.user;
+    return this.calendarService.updateEvent(updateEventDto, user.id);
   }
 
   @ApiTags('API')
   @ApiResponse({ status: HttpStatus.OK, type: CalendarResponse })
+  @UseGuards(JwtAuthGuard)
   @Header('Content-type', 'application/json')
   @HttpCode(HttpStatus.OK)
   @Delete(':id')
-  deleteEvent(@Param('id') id: string): Promise<CalendarResponse> {
-    return this.calendarService.deleteEvent(id);
+  deleteEvent(
+    @Param('id') id: string,
+    @Req() request,
+  ): Promise<CalendarResponse> {
+    const user: UserDTO = request.user;
+    return this.calendarService.deleteEvent(+id, user.id);
   }
 }
