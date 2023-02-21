dcc2edd199d46a787f0f396226796d9ff9611c90
XCrone <ls.xcrone@gmail.com>
2023-02-21 17:39:40 +0300

    feature: add guards and refactor

--- src/modules/todos/todos.controller.ts
+++ src/modules/todos/todos.controller.ts
@@ -11,10 +11,14 @@ import {
   Param,
   Patch,
   Post,
+  Req,
+  UseGuards,
 } from '@nestjs/common';
 import { TodosService } from './todos.service';
 import { ApiResponse, ApiTags } from '@nestjs/swagger';
 import { TodoResponse } from './response/todo.respose';
+import { JwtAuthGuard } from 'src/guards/jwt.guard';
+import { UserDTO } from '../auth/dto/user.dto';
 
 @Controller('todos')
 export class TodosController {
@@ -22,49 +26,63 @@ export class TodosController {
 
   @ApiTags('API')
   @ApiResponse({ status: HttpStatus.OK, type: Array<TodoResponse> })
+  @UseGuards(JwtAuthGuard)
   @Header('Content-type', 'application/json')
   @HttpCode(HttpStatus.OK)
   @Get()
-  getTodos(): Promise<TodoResponse[]> {
-    return this.todosService.getTodos();
+  getTodos(@Req() request): Promise<TodoResponse[]> {
+    const user: UserDTO = request.user;
+    return this.todosService.getTodos(user.id);
   }
 
   @ApiTags('API')
   @ApiResponse({ status: HttpStatus.OK, type: TodoResponse })
+  @UseGuards(JwtAuthGuard)
   @Header('Content-type', 'application/json')
   @HttpCode(HttpStatus.OK)
   @Get(':id')
-  getOneTodo(@Param('id') id: string): Promise<TodoResponse> {
-    return this.todosService.getOneTodo(id);
+  getOneTodo(@Param('id') id: string, @Req() request): Promise<TodoResponse> {
+    const user: UserDTO = request.user;
+    return this.todosService.getOneTodo(+id, user.id);
   }
 
   @ApiTags('API')
   @ApiResponse({ status: HttpStatus.CREATED, type: TodoResponse })
+  @UseGuards(JwtAuthGuard)
   @Header('Content-type', 'application/json')
   @HttpCode(HttpStatus.CREATED)
   @Post()
-  createTodo(@Body() createTodoDto: CreateTodoDTO): Promise<TodoResponse> {
-    return this.todosService.createTodo(createTodoDto);
+  createTodo(
+    @Body() createTodoDto: CreateTodoDTO,
+    @Req() request,
+  ): Promise<TodoResponse> {
+    const user: UserDTO = request.user;
+    console.log(user);
+    return this.todosService.createTodo(createTodoDto, user.id);
   }
 
   @ApiTags('API')
   @ApiResponse({ status: HttpStatus.OK, type: TodoResponse })
+  @UseGuards(JwtAuthGuard)
   @Header('Content-type', 'application/json')
   @HttpCode(HttpStatus.OK)
-  @Patch(':id')
+  @Patch()
   updateTodo(
     @Body() updateTodoDto: UpdateTodoDTO,
-    @Param('id') id: string,
+    @Req() request,
   ): Promise<TodoResponse> {
-    return this.todosService.updateTodo(updateTodoDto, id);
+    const user: UserDTO = request.user;
+    return this.todosService.updateTodo(updateTodoDto, user.id);
   }
 
   @ApiTags('API')
   @ApiResponse({ status: HttpStatus.OK, type: TodoResponse })
+  @UseGuards(JwtAuthGuard)
   @Header('Content-type', 'application/json')
   @HttpCode(HttpStatus.OK)
   @Delete(':id')
-  deleteTodo(@Param('id') id: string): Promise<TodoResponse> {
-    return this.todosService.deleteTodo(id);
+  deleteTodo(@Param('id') id: string, @Req() request): Promise<TodoResponse> {
+    const user: UserDTO = request.user;
+    return this.todosService.deleteTodo(+id, user.id);
   }
 }
