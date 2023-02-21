41e9f4a92329eaef6cc6374b099714c143e5a4c1
XCrone <ls.xcrone@gmail.com>
2023-02-21 17:40:23 +0300

    feature: search by uid

--- src/modules/todos/todos.service.ts
+++ src/modules/todos/todos.service.ts
@@ -1,48 +1,50 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, BadRequestException } from '@nestjs/common';
 import { CreateTodoDTO } from './dto/create-todo.dto';
 import { UpdateTodoDTO } from './dto/update-todo.dto';
 import { InjectModel } from '@nestjs/sequelize';
 import { Todo } from './models/todo.model';
 import { TodoResponse } from './response/todo.respose';
+import { APP_ERRORS } from 'src/common/errors';
 
 @Injectable()
 export class TodosService {
   constructor(@InjectModel(Todo) private todoRepository: typeof Todo) {}
 
-  async getTodos(): Promise<TodoResponse[]> {
-    return (await this.todoRepository.findAll()) as TodoResponse[];
+  async getTodos(uid: number): Promise<TodoResponse[]> {
+    return (await this.todoRepository.findAll({
+      where: { uid },
+    })) as TodoResponse[];
   }
 
-  async getOneTodo(id: string): Promise<TodoResponse | undefined> {
-    return (await this.todoRepository.findOne({
+  async getOneTodo(id: number, uid: number): Promise<TodoResponse | undefined> {
+    const todo = (await this.todoRepository.findOne({
       where: {
         id,
+        uid,
       },
     })) as TodoResponse;
+
+    if (todo) {
+      return todo;
+    }
+
+    throw new BadRequestException(APP_ERRORS.TODO_NOT_FOUND);
   }
 
-  async createTodo(dto: CreateTodoDTO): Promise<TodoResponse> {
+  async createTodo(dto: CreateTodoDTO, uid: number): Promise<TodoResponse> {
     const todo = new Todo();
+    todo.uid = uid;
     Object.assign(todo, dto);
     return (await todo.save()) as TodoResponse;
   }
 
-  async deleteTodo(id: string): Promise<TodoResponse> {
-    const todo = await this.todoRepository.findOne({
-      where: {
-        id,
-      },
-    });
-    await todo.destroy();
-    return todo as TodoResponse;
-  }
-
-  async updateTodo(dto: UpdateTodoDTO, id: string): Promise<TodoResponse> {
+  async updateTodo(dto: UpdateTodoDTO, uid: number): Promise<TodoResponse> {
     const result = await this.todoRepository.update(
       { ...dto },
       {
         where: {
-          id,
+          id: dto.id,
+          uid,
         },
         returning: true,
       },
@@ -51,6 +53,26 @@ export class TodosService {
     const [count, item] = result;
     const payload = item[0] as TodoResponse;
 
-    return payload;
+    if (payload) {
+      return payload;
+    }
+
+    throw new BadRequestException(APP_ERRORS.TODO_NOT_FOUND);
+  }
+
+  async deleteTodo(id: number, uid: number): Promise<TodoResponse> {
+    const todo = await this.todoRepository.findOne({
+      where: {
+        id,
+        uid,
+      },
+    });
+
+    if (todo) {
+      await todo.destroy();
+      return todo as TodoResponse;
+    }
+
+    throw new BadRequestException(APP_ERRORS.TODO_NOT_FOUND);
   }
 }
