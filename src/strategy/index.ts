98d741ef170e189c6033845c001f5a80a6caee04
XCrone <ls.xcrone@gmail.com>
2023-02-21 14:48:03 +0300

    feature: create strategy

---
+++ src/strategy/index.ts
@@ -0,0 +1,19 @@
+import { Injectable } from '@nestjs/common';
+import { ConfigService } from '@nestjs/config';
+import { PassportStrategy } from '@nestjs/passport';
+import { Strategy, ExtractJwt } from 'passport-jwt';
+
+@Injectable()
+export class JWTStrategy extends PassportStrategy(Strategy) {
+  constructor(private readonly configService: ConfigService) {
+    super({
+      jwtFromRequest: ExtractJwt.fromAuthHeaderAsBearerToken(),
+      ignoreExpiration: false,
+      secretOrKey: configService.get('jwt_secret'),
+    });
+  }
+
+  async validate(payload: any) {
+    return { ...payload.user };
+  }
+}
