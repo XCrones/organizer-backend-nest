9c210154a290f330b375dc7ef23790c4272d0922
XCrone <ls.xcrone@gmail.com>
2023-02-21 13:31:03 +0300

    feature: add api swagger

--- src/modules/todos/todos.controller.ts
+++ src/modules/todos/todos.controller.ts
@@ -1,5 +1,5 @@
-import { UpdateTodoDto } from './dto/update-todo.dto';
-import { CreateTodoDto } from './dto/create-todo.dto';
+import { UpdateTodoDTO } from './dto/update-todo.dto';
+import { CreateTodoDTO } from './dto/create-todo.dto';
 import {
   Body,
   Controller,
@@ -13,35 +13,54 @@ import {
   Post,
 } from '@nestjs/common';
 import { TodosService } from './todos.service';
+import { ApiResponse, ApiTags } from '@nestjs/swagger';
+import { TodoResponse } from './response/todo.respose';
 
 @Controller('todos')
 export class TodosController {
   constructor(private readonly todosService: TodosService) {}
 
+  @ApiTags('API')
+  @ApiResponse({ status: HttpStatus.OK, type: Array<TodoResponse> })
+  @HttpCode(HttpStatus.OK)
   @Get()
-  getTodos() {
+  getTodos(): Promise<TodoResponse[]> {
     return this.todosService.getTodos();
   }
 
+  @ApiTags('API')
+  @ApiResponse({ status: HttpStatus.OK, type: TodoResponse })
   @Get(':id')
-  getOneTodo(@Param('id') id: string) {
+  @HttpCode(HttpStatus.OK)
+  getOneTodo(@Param('id') id: string): Promise<TodoResponse> {
     return this.todosService.getOneTodo(id);
   }
 
+  @ApiTags('API')
+  @ApiResponse({ status: HttpStatus.CREATED, type: TodoResponse })
   @Post()
   @HttpCode(HttpStatus.CREATED)
   @Header('Content-type', 'application/json')
-  createTodo(@Body() createTodoDto: CreateTodoDto) {
+  createTodo(@Body() createTodoDto: CreateTodoDTO): Promise<TodoResponse> {
     return this.todosService.createTodo(createTodoDto);
   }
 
+  @ApiTags('API')
+  @ApiResponse({ status: HttpStatus.OK, type: TodoResponse })
   @Patch(':id')
-  updateTodo(@Body() updateTodoDto: UpdateTodoDto, @Param('id') id: string) {
+  @HttpCode(HttpStatus.OK)
+  updateTodo(
+    @Body() updateTodoDto: UpdateTodoDTO,
+    @Param('id') id: string,
+  ): Promise<TodoResponse> {
     return this.todosService.updateTodo(updateTodoDto, id);
   }
 
+  @ApiTags('API')
+  @ApiResponse({ status: HttpStatus.OK, type: TodoResponse })
   @Delete(':id')
-  deleteTodo(@Param('id') id: string) {
+  @HttpCode(HttpStatus.OK)
+  deleteTodo(@Param('id') id: string): Promise<TodoResponse> {
     return this.todosService.deleteTodo(id);
   }
 }
