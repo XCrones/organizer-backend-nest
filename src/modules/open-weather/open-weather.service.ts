b2bde51f8ce4c130c8f1e379ad3c95efd6159605
XCrone <ls.xcrone@gmail.com>
2023-03-02 18:49:08 +0300

    delete fetch forecast & promise res any

--- src/modules/open-weather/open-weather.service.ts
+++ src/modules/open-weather/open-weather.service.ts
@@ -4,8 +4,8 @@ import { ConfigService } from '@nestjs/config';
 import { AxiosError } from 'axios';
 import { APP_ERRORS } from 'src/common/errors';
 import { catchError, firstValueFrom } from 'rxjs';
-import { WeatherCurrentApiDTO } from './dto/weather-current-api.dto';
-import { WeatherForecastDTO } from './dto/weather-forecast.dto';
+
+export type TParamReq = 'weather' | 'forecast';
 
 @Injectable()
 export class OpenWeatherService {
@@ -14,41 +14,26 @@ export class OpenWeatherService {
     private readonly httpService: HttpService,
   ) {}
 
-  async fetchCurrent(name: string): Promise<WeatherCurrentApiDTO> {
-    const urlApi = this.configService.get('weather_url_api');
-    const token = this.configService.get('weather_token');
-    const { data } = await firstValueFrom(
-      this.httpService
-        .get<WeatherCurrentApiDTO>(
-          `${urlApi}/weather?q=${name}&units=metric&appid=${token}`,
-        )
-        .pipe(
-          catchError((error: AxiosError) => {
-            //! throw error;
-            throw new BadRequestException(APP_ERRORS.CITY_NOT_FOUND);
-          }),
-        ),
-    );
+  async fetchWeather(name: string, reqParam: TParamReq): Promise<any> {
+    if (!!name) {
+      const urlApi = this.configService.get('weather_url_api');
+      const token = this.configService.get('weather_token');
 
-    return data;
-  }
+      const { data } = await firstValueFrom(
+        this.httpService
+          .get<any>(
+            `${urlApi}/${reqParam}?q=${name}&units=metric&appid=${token}`,
+          )
+          .pipe(
+            catchError((error: AxiosError) => {
+              //! throw error;
+              throw new BadRequestException(APP_ERRORS.CITY_NOT_FOUND);
+            }),
+          ),
+      );
 
-  async fetchForecast(name: string): Promise<WeatherForecastDTO> {
-    const urlApi = this.configService.get('weather_url_api');
-    const token = this.configService.get('weather_token');
-    const { data } = await firstValueFrom(
-      this.httpService
-        .get<WeatherForecastDTO>(
-          `${urlApi}/forecast?q=${name}&units=metric&appid=${token}`,
-        )
-        .pipe(
-          catchError((error: AxiosError) => {
-            //! throw error;
-            throw new BadRequestException(APP_ERRORS.CITY_NOT_FOUND);
-          }),
-        ),
-    );
-
-    return data;
+      return data;
+    }
+    throw new BadRequestException(APP_ERRORS.CITY_EMPTY_NAME);
   }
 }
