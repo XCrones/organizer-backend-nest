754668b4a87bd7fa9425f27074420f426f61df88
XCrone <ls.xcrone@gmail.com>
2023-03-02 16:51:15 +0300

    rename: to current service & add forecast service

--- src/modules/weather/weather.controller.ts
+++ src/modules/weather/weather.controller.ts
@@ -1,4 +1,4 @@
-import { WeatherService } from './weather.service';
+import { WeatherCurrentService } from './weather-current.service';
 import {
   Body,
   Controller,
@@ -10,19 +10,35 @@ import {
 } from '@nestjs/common';
 import { ApiResponse, ApiTags } from '@nestjs/swagger';
 import { JwtAuthGuard } from 'src/guards/jwt.guard';
-import { GetCityDTO } from './dto/get-city.dto';
-import { WeatherResponse } from './response/response';
+import { GetWeatherDTO } from './dto/get-weather.dto';
+import { CurrentResponse } from './response/current.response';
+import { WeatherForecastService } from './weather-forecast.service';
+import { ForecastResponse } from './response/forecast.response';
 
 @Controller('weather')
 export class WeatherController {
-  constructor(private readonly weatherService: WeatherService) {}
+  constructor(
+    private readonly weatherCurrentService: WeatherCurrentService,
+    private readonly weatherForecastService: WeatherForecastService,
+  ) {}
+
   @ApiTags('API')
-  @ApiResponse({ status: HttpStatus.OK, type: WeatherResponse })
+  @ApiResponse({ status: HttpStatus.OK, type: CurrentResponse })
   @UseGuards(JwtAuthGuard)
   @Header('Content-type', 'application/json')
   @HttpCode(HttpStatus.OK)
-  @Get()
-  getWeather(@Body() dto: GetCityDTO) {
-    return this.weatherService.getCity(dto);
+  @Get('current')
+  getCurrent(@Body() dto: GetWeatherDTO) {
+    return this.weatherCurrentService.getCurrent(dto);
+  }
+
+  @ApiTags('API')
+  @ApiResponse({ status: HttpStatus.OK, type: ForecastResponse })
+  @UseGuards(JwtAuthGuard)
+  @Header('Content-type', 'application/json')
+  @HttpCode(HttpStatus.OK)
+  @Get('forecast')
+  getForecast(@Body() dto: GetWeatherDTO) {
+    return this.weatherForecastService.getForecast(dto);
   }
 }
