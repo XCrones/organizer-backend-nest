f96845ecd4c62e66ed92119a9768dfd111a885f1
XCrone <ls.xcrone@gmail.com>
2023-03-02 10:40:57 +0300

    rm: httpModule

--- src/modules/weather/weather.module.ts
+++ src/modules/weather/weather.module.ts
@@ -1,18 +1,12 @@
+import { OpenWeatherModule } from './../../open-weather/open-weather.module';
 import { Module } from '@nestjs/common';
 import { WeatherController } from './weather.controller';
 import { WeatherService } from './weather.service';
 import { Weather } from './models/weather.model';
 import { SequelizeModule } from '@nestjs/sequelize';
-import { HttpModule } from '@nestjs/axios';
 
 @Module({
-  imports: [
-    SequelizeModule.forFeature([Weather]),
-    HttpModule.register({
-      timeout: 5000,
-      maxRedirects: 1,
-    }),
-  ],
+  imports: [OpenWeatherModule, SequelizeModule.forFeature([Weather])],
   controllers: [WeatherController],
   providers: [WeatherService],
 })
