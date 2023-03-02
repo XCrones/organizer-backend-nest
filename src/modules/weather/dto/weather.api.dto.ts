8104eec47a64673f04f5851ea1a7b5cc2f5d26c3
XCrone <ls.xcrone@gmail.com>
2023-03-02 10:41:11 +0300

    rm: dto

--- src/modules/weather/dto/weather.api.dto.ts
+++
@@ -1,9 +0,0 @@
-import { ApiProperty } from '@nestjs/swagger';
-import { IsNumber } from 'class-validator';
-import { WeatherDTO } from './weather.dto';
-
-export class WeatherApiDTO extends WeatherDTO {
-  @ApiProperty()
-  @IsNumber()
-  id: number;
-}
