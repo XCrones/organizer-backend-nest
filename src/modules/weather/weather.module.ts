9617afc739f5b8270c3177ed8c90c6785bfad94b
XCrone <ls.xcrone@gmail.com>
2023-03-02 11:29:02 +0300

    change: path

--- src/modules/weather/weather.module.ts
+++ src/modules/weather/weather.module.ts
@@ -1,9 +1,9 @@
-import { OpenWeatherModule } from './../../open-weather/open-weather.module';
 import { Module } from '@nestjs/common';
 import { WeatherController } from './weather.controller';
 import { WeatherService } from './weather.service';
 import { Weather } from './models/weather.model';
 import { SequelizeModule } from '@nestjs/sequelize';
+import { OpenWeatherModule } from '../open-weather/open-weather.module';
 
 @Module({
   imports: [OpenWeatherModule, SequelizeModule.forFeature([Weather])],
