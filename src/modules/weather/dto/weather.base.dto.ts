5cf942b3bb7d2edd68aedc54dfebda24c1f33dc1
XCrone <ls.xcrone@gmail.com>
2023-03-02 11:16:44 +0300

    change: path open weather DTO

--- src/modules/weather/dto/weather.base.dto.ts
+++ src/modules/weather/dto/weather.base.dto.ts
@@ -1,6 +1,6 @@
 import { ApiProperty } from '@nestjs/swagger';
 import { IsNumber } from 'class-validator';
-import { WeatherDTO } from 'src/open-weather/dto/weather.dto';
+import { WeatherDTO } from 'src/modules/open-weather/dto/weather.dto';
 
 export class WeatherBaseDTO extends WeatherDTO {
   @ApiProperty()
