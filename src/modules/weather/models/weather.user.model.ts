714abd0b1ab5185ff7a4db2d421ce918d0d39ae9
XCrone <ls.xcrone@gmail.com>
2023-03-06 11:52:04 +0300

    join: model

--- src/modules/weather/models/weather.user.model.ts
+++ src/modules/weather/models/weather.user.model.ts
@@ -1,10 +1,10 @@
-import { Column, Model, Table } from 'sequelize-typescript';
+import { Column, DataType, Model, Table } from 'sequelize-typescript';
 
 @Table
 export class WeatherUser extends Model {
   @Column
   uid: number;
 
-  @Column
-  cityID: number;
+  @Column(DataType.JSONB)
+  citiesId: number[];
 }
