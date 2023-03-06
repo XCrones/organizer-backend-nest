ba0e681b302e7a9d4b2e2779626d8c4e9c2b3fe8
XCrone <ls.xcrone@gmail.com>
2023-03-06 11:57:11 +0300

    rm: WeatherCurrent

--- src/modules/weather/dto/weather-current.base.dto.ts
+++
@@ -1,9 +0,0 @@
-import { ApiProperty } from '@nestjs/swagger';
-import { IsNumber } from 'class-validator';
-import { WeatherCurrentDTO } from 'src/modules/open-weather/dto/weather-current.dto';
-
-export class WeatherCurrentBaseDTO extends WeatherCurrentDTO {
-  @ApiProperty()
-  @IsNumber()
-  cityID: number;
-}
