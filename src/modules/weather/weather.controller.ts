5ef111e5426d2fde30a0756b1be0ed691ebb3fa0
XCrone <ls.xcrone@gmail.com>
2023-03-06 11:51:44 +0300

    add: dropCity

--- src/modules/weather/weather.controller.ts
+++ src/modules/weather/weather.controller.ts
@@ -2,10 +2,13 @@ import { WeatherCurrentService } from './weather-current.service';
 import {
   Body,
   Controller,
+  Delete,
   Get,
   Header,
   HttpCode,
   HttpStatus,
+  Param,
+  Req,
   UseGuards,
 } from '@nestjs/common';
 import { ApiResponse, ApiTags } from '@nestjs/swagger';
@@ -14,12 +17,15 @@ import { GetWeatherDTO } from './dto/get-weather.dto';
 import { CurrentResponse } from './response/current.response';
 import { WeatherForecastService } from './weather-forecast.service';
 import { ForecastResponse } from './response/forecast.response';
+import { UserDTO } from '../auth/dto/user.dto';
+import { WeatherUserService } from './services/weather-user.service';
 
 @Controller('weather')
 export class WeatherController {
   constructor(
     private readonly weatherCurrentService: WeatherCurrentService,
     private readonly weatherForecastService: WeatherForecastService,
+    private readonly weatherUserService: WeatherUserService,
   ) {}
 
   @ApiTags('API')
@@ -37,8 +43,24 @@ export class WeatherController {
   @UseGuards(JwtAuthGuard)
   @Header('Content-type', 'application/json')
   @HttpCode(HttpStatus.OK)
-  @Get('forecast')
-  getForecast(@Body() dto: GetWeatherDTO): Promise<ForecastResponse> {
-    return this.weatherForecastService.getForecast(dto);
+  @Get()
+  async getForecast(
+    @Body() dto: GetWeatherDTO,
+    @Req() request,
+  ): Promise<ForecastResponse> {
+    const user: UserDTO = request.user;
+    const weather = await this.weatherForecastService.getForecast(dto);
+    return this.weatherUserService.addCity(user.id, weather);
+  }
+
+  @ApiTags('API')
+  @ApiResponse({ status: HttpStatus.OK, type: Array<number> })
+  @UseGuards(JwtAuthGuard)
+  @Header('Content-type', 'application/json')
+  @HttpCode(HttpStatus.OK)
+  @Delete(':cityId')
+  dropCity(@Param('cityId') cityId: number, @Req() request): Promise<number[]> {
+    const user: UserDTO = request.user;
+    return this.weatherUserService.dropCity(user.id, +cityId);
   }
 }
